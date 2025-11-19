335cd3aaa275969f4b9582a8c5d45bf0083366a2
Your Name <you@example.com>
2025-11-19 17:41:54 +0900

    RPi.GPIO 설정 및 PWM 제어 코드 추가

---
+++ motor/test.py
@@ -0,0 +1,17 @@
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+
+pwm_pin = 18
+in1_pin = 23
+in2_pin = 24
+
+GPIO.setup(pwm_pin, GPIO.OUT)
+GPIO.setup(in1_pin, GPIO.OUT)
+GPIO.setup(in2_pin, GPIO.OUT)
+
+pwm = GPIO.PWM(pwm_pin, 1000)  # 1 kHz frequency
+pwm.start(0)
+
+GPIO.output(in1_pin, True)
+GPIO.output(in2_pin, False)
