49545c0447df3b00e89f404e084199c4ce203700
Your Name <you@example.com>
2025-11-19 19:56:21 +0900

    예외 처리를 추가하여 PWM 제어 로직 안정성 향상

--- motor/test.py
+++ motor/test.py
@@ -1,4 +1,5 @@
 import RPi.GPIO as GPIO
+import time
 
 GPIO.setmode(GPIO.BCM)
 
@@ -10,9 +11,20 @@ GPIO.setup(pwm_pin, GPIO.OUT)
 GPIO.setup(in1_pin, GPIO.OUT)
 GPIO.setup(in2_pin, GPIO.OUT)
 
-pwm = GPIO.PWM(pwm_pin, 1000)  # 1 kHz frequency
+pwm = GPIO.PWM(pwm_pin, 1000)  # 1 kHz
 pwm.start(0)
 
-while True :
+try:
     GPIO.output(in1_pin, True)
     GPIO.output(in2_pin, False)
+    pwm.ChangeDutyCycle(70)  # 듀티 사이클 70%
+
+    while True:
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    pass
+
+finally:
+    pwm.stop()
+    GPIO.cleanup()
