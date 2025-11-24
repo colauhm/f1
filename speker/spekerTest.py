9ff716a2fa6aab78b2af726a5bad7ecb2d44d322
Your Name <you@example.com>
2025-11-24 16:43:31 +0900

    스피커 카드번호 수정

--- speker/spekerTest.py
+++ speker/spekerTest.py
@@ -5,7 +5,7 @@ def set_volume(level):
     level: 0 ~ 100 사이의 정수
     """
     # 카드 번호(-c 1)와 컨트롤 이름(PCM)은 네 환경에 맞게 수정해야 함
-    os.system(f"amixer -c 1 sset PCM {level}% > /dev/null")
+    os.system(f"amixer -c 3 sset PCM {level}% > /dev/null")
 
 # 사용 예시
 print("안전 모드 발동! 볼륨 최대")
