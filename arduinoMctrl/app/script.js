533d30b7ed1cf4a429cf0a0283bd7cbefd3251fc
Your Name <you@example.com>
2025-12-03 21:14:52 +0900

    세부 값 수정

--- arduinoMctrl/app/script.js
+++ arduinoMctrl/app/script.js
@@ -20,7 +20,7 @@ const THRESHOLD_COUNT = 3;
 const COLOR_GREEN = "#39ff14";
 const COLOR_RED = "#ff0000";
 
-const TIRE_CIRCUM = 2.0; 
+const TIRE_CIRCUM = 1.0; 
 
 let pedalBuffer = [], motorBuffer = [], velocityBuffer = [], distanceBuffer = [];
 const MAX_STORE_MINUTES = 1.5; 
